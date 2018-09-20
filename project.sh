45646e9d7af05f6b1e01d9b4248b663b06691ba0
cezarmathe <cezarmathe@gmail.com>
2018-09-20 21:15:48 +0300

    Scaffolding for arg parsing

--- project.sh
+++ project.sh
@@ -1,1 +1,21 @@
 #!/bin/bash
+
+# Parsing args
+while (( "$#" )); do
+    case "$1" in
+        -c|--config)
+
+            shift 3
+            ;;
+        --)
+            shift
+            break
+        -*|--*=)
+            echo "Unknown flag: $1" >&2
+            exit 1
+            ;;
+        *)
+            shift
+            ;;
+    esac
+done
